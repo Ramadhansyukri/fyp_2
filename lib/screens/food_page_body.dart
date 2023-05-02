dbc5c680a312ad3d1989469cc3b55613d0cd140c
Ahmad Ramadhan <ahmadramadhansyukri@graduate.utm.my>
2023-05-02 14:44:42 +0800

    Adjustment to home screen

---
+++ lib/screens/food_page_body.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:fyp_2/utils/colors.dart';
+import 'package:fyp_2/widgets/big_text.dart';
+import 'package:fyp_2/widgets/small_text.dart';
+
+class FoodPageBody extends StatefulWidget {
+  const FoodPageBody({Key? key}) : super(key: key);
+
+  @override
+  State<FoodPageBody> createState() => _FoodPageBodyState();
+}
+
+class _FoodPageBodyState extends State<FoodPageBody> {
+  PageController pageController = PageController(viewportFraction: 0.9);
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.redAccent,
+      height: 320,
+      child: PageView.builder(
+          controller: pageController,
+          itemCount: 5,
+          itemBuilder: (context, position){
+            return _buildPageItem(position);
+          }),
+    );
+  }
+  Widget _buildPageItem(int index){
+    return Stack(
+      children: [
+        Container(
+          height: 220,
+          margin: EdgeInsets.only(left: 5, right: 5),
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(30),
+              color: index.isEven?Color(0xFF69c5df):Color(0xff9294cc),
+              image: DecorationImage(
+                  fit: BoxFit.cover,
+                  image: AssetImage(
+                      "assets/image/Food1.png"
+                  )
+              )
+          ),
+        ),
+        Align(
+          alignment: Alignment.bottomCenter,
+          child: Container(
+            height: 120,
+            margin: EdgeInsets.only(left: 30, right: 30, bottom: 15),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(30),
+              color: Colors.white,
+
+            ),
+            child: Container(
+              padding: EdgeInsets.only(top: 15, left: 15, right: 15),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  BigText(text: "Nasi Lemak"),
+                  SizedBox(height: 10,),
+                  Row(
+                    children: [
+                      Wrap(
+                        children: List.generate(5, (index) => Icon(Icons.star, color: AppColors.mainColor, size: 15)),
+                      ),
+                      SizedBox(width: 10,),
+                      SmallText(text: "4.5"),
+                      SizedBox(width: 10,),
+                      SmallText(text: "1287"),
+                      SizedBox(width: 10,),
+                      SmallText(text: "comments")
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ),
+        )
+      ],
+    );
+  }
+}
--- lib/screens/user_home_screen.dart
+++ lib/screens/user_home_screen.dart
@@ -1,14 +1,26 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:fyp_2/screens/food_page_body.dart';
+import 'package:fyp_2/widgets/big_text.dart';
+import 'package:fyp_2/widgets/small_text.dart';
 import 'package:fyp_2/services/auth.dart';
 
-class UserHome extends StatelessWidget {
+import '../utils/colors.dart';
+
+class UserHome extends StatefulWidget {
+  const UserHome({Key? key}) : super(key: key);
+
+  @override
+  State<UserHome> createState() => _UserHomeState();
+}
+
+class _UserHomeState extends State<UserHome> {
 
   final AuthService _auth = AuthService();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.pink[600],
       appBar: AppBar(
         title: Text('Home'),
         backgroundColor: Colors.pink[900],
@@ -21,6 +33,45 @@ class UserHome extends StatelessWidget {
               await _auth.SignOut();
             },
           )
+        ],
+      ),
+      body: Column(
+        children: [
+          Container(
+
+            child: Container(
+              margin: EdgeInsets.only(top: 45, bottom: 15),
+              padding: EdgeInsets.only(left: 20, right: 20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    children: [
+                      BigText(text: "Malaysia", color: AppColors.mainColor,size: 30,),
+                      Row(
+                        children: [
+                          SmallText(text: "Johor", color: Colors.black54,),
+                          Icon(Icons.arrow_drop_down_rounded)
+                        ],
+                      )
+                    ],
+                  ),
+                  Center(
+                    child: Container(
+                      width: 45,
+                      height: 45,
+                      child: Icon(Icons.search, color: Colors.white),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(15),
+                        color: AppColors.mainColor,
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+          FoodPageBody(),
         ],
       ),
     );
